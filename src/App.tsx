a2e6ef93510b577cf93e2dc431eba7d951f3749a
Salva Cheung <salva.cheung@outlook.com>
2023-09-25 19:52:32 +0800

    feat: plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { schema } from "prosemirror-schema-basic";
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
+import { undo, redo, history } from "prosemirror-history";
+// keymap 插件, 它用来绑定键盘输入的 actions.
+import { keymap } from "prosemirror-keymap";
 import { useRef, useEffect } from "react";
 
 function App() {
@@ -8,7 +11,11 @@ function App() {
 
   useEffect(() => {
     if (editorBox.current) {
-      const state = EditorState.create({ schema });
+      const state = EditorState.create({
+        schema,
+        plugins: [history(), keymap({ "Mod-z": undo, "Mod-y": redo })],
+      });
+
       const editor = new EditorView(editorBox.current, {
         state,
         dispatchTransaction(transaction) {
@@ -18,6 +25,8 @@ function App() {
             "to",
             transaction.doc.content,
           );
+          // 如果注释下方两行代码，即不应用事务变化，在输入框中将看不到内容
+          // 但是仍然可以监测到事务变化信息
           const newState = editor.state.apply(transaction);
           editor.updateState(newState);
         },
