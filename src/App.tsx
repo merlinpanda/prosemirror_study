2609786eef98f737e10b4309cc7b631e0c5aaea4
Salva Cheung <salva.cheung@outlook.com>
2023-09-25 19:39:36 +0800

    feat: prosemirror transaction
    prosemirror 事务，每次的state更新，都会执行updateState方法，而且每次派发一个事务一般情况下都会触发编辑状态的更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
+import { schema } from "prosemirror-schema-basic";
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
-import { Schema, DOMParser } from "prosemirror-model";
-import { schema } from "prosemirror-schema-basic";
-import { addListNodes } from "prosemirror-schema-list";
-import { exampleSetup } from "prosemirror-example-setup";
 import { useRef, useEffect } from "react";
 
 function App() {
   const editorBox = useRef(null);
-  const editorContent = useRef(null);
 
   useEffect(() => {
-    if (editorBox.current && editorContent.current) {
-      const mySchema = new Schema({
-        nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-        marks: schema.spec.marks,
-      });
-
+    if (editorBox.current) {
+      const state = EditorState.create({ schema });
       const editor = new EditorView(editorBox.current, {
-        state: EditorState.create({
-          doc: DOMParser.fromSchema(mySchema).parse(editorContent.current),
-          plugins: exampleSetup({ schema: mySchema }),
-        }),
+        state,
+        dispatchTransaction(transaction) {
+          console.log(
+            "Document size went from",
+            transaction.before.content,
+            "to",
+            transaction.doc.content,
+          );
+          const newState = editor.state.apply(transaction);
+          editor.updateState(newState);
+        },
       });
 
       return () => {
@@ -32,11 +31,8 @@ function App() {
 
   return (
     <>
-      <div ref={editorBox}>
-        <div ref={editorContent}>
-          <div className="">aaa</div>
-          <div className="">aaaasd</div>
-        </div>
+      <div className="">
+        <div ref={editorBox}></div>
       </div>
     </>
   );
